59b9d55fbb0f98d19ea79e8c722fa062e8aeddba
agustinferens-spec <agustin.ferens@gmail.com>
2026-09-03 19:09:55 -0300

    Actualizo Funciones.py

---
+++ Funciones.py
@@ -0,0 +1,14 @@
+# =========================
+# FUNCIONES
+# =========================
+import os
+
+def mostrar_menu():
+    print("\n===== GPS AUTOMOTORES =====")
+    print("1. Registrar vehículo")
+    print("2. Ver vehículos disponibles")
+    print("3. Buscar vehículo")
+    print("4. Registrar venta")
+    print("5. Ver vehículos vendidos")
+    print("6. Salir")
+
