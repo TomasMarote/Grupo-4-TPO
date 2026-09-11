4134503cb36f5d854387d3709db39b2a3b93542c
agustinferens-spec <agustin.ferens@gmail.com>
2026-09-11 19:08:50 -0300

    acomodo de espacios

--- Funciones.py
+++ Funciones.py
@@ -153,10 +153,10 @@ def registrar_vehiculo(vehiculos):
     print("ID:", id_vehiculo)
     print("Marca:", marca)
     print("Modelo:", modelo)
-    print("Precio:", moneda, precio)
+    print("Precio:", moneda, precio) 
+
     
 def buscar_vehiculo(vehiculos):
-    def buscar_vehiculo(vehiculos):
 
     print("\n========== BUSCAR VEHÍCULO ==========")
 
